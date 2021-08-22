ef83f6b5cf7d00c1068cbda2b75e82e901ef3b7f
Blshop <envworking@gmail.com>
2021-08-22 18:00:43 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let letter = ''
+    let phrase = ''
+    for (let i=0; i<expr.length;i+=10){
+        letter = expr.slice(i,i+10)
+        if (letter[0]=='*'){
+            phrase +=' '
+        }
+        else{
+            let morse = ''
+            for (let j=0; j<letter.length; j+=2){
+                switch (letter.slice(j,j+2)){
+                    case '00':
+                        break
+                    case '10':
+                        morse +='.'
+                        break
+                    case '11':
+                        morse +='-'
+                        break
+                }
+            }
+            phrase += MORSE_TABLE[morse]
+        }
+    }
+    return phrase
 }
 
 module.exports = {
